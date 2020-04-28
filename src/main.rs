f4339861f065228aabe356e5cf5de58ccd907a4c
Orin Serimon <orin220444@protonmail.com>
2020-04-28 12:56:28 +0000

    [WIP:] full convertation from json to rust

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use structopt::StructOpt;
+extern crate serde;
+extern crate serde_json;
+
 /// searches links in telegram message
 #[derive(StructOpt)]
 struct Cli{
@@ -8,10 +11,26 @@ path: std::path::PathBuf,
 }
 fn main() {
 let args = Cli::from_args();
+///get all the data from the file to a variable
 let content = std::fs::read_to_string(&args.path)
 .expect("could not read file");
 println!("{}", content);
-let entities: serde_json::Value = 
-serde_json::from_str(&content).expect("JSON not well-formatted");
-println!("entities: {}",entities)
+/// create a struct to clone data from the object in the array
+use serde_json::Number;
+struct Entity {
+offset: Number,
+length: Number,
+type_is: String,
+url:String,
+}
+/// create a vec of structs of entities
+use std::fmt::Debug;
+#[derive(Debug)]
+let entities: Vec<Entity> = 
+serde_json::from_str(&content)?;//.expect("JSON not well-formatted");
+println!("entities: {:#?}",entities);
+for i in entities {
+println!("{:#?}", [i]);
+}
+
 }
